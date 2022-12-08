d6e9a4e599aa655fb66eaf6849c5318c087e7522
AALab_linziyao <46914247+EKiritsugu@users.noreply.github.com>
2022-12-08 16:43:33 +0800

    。

--- plot_test.py
+++ plot_test.py
@@ -1,5 +1,5 @@
 # "C:/Users/aalab_linziyao/Documents/GitHub/jisamm/data/20221207-180003_reverb_interf_performance_2fd4afb7fe/data.json"
-dir = ['data/20221208-160601_reverb_interf_performance_135960f7b2/']
+dir = ['data/20221208-163059_reverb_interf_performance_8d8a980436/']
 
 
 import json
