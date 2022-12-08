3d22b88258c4c37071905b7d4c2a282cc65c2eb7
AALab_linziyao <46914247+EKiritsugu@users.noreply.github.com>
2022-12-08 17:10:13 +0800

    这一波代码对了，存档作为参考

--- plot_test.py
+++ plot_test.py
@@ -1,5 +1,5 @@
 # "C:/Users/aalab_linziyao/Documents/GitHub/jisamm/data/20221207-180003_reverb_interf_performance_2fd4afb7fe/data.json"
-dir = ['data/20221208-163059_reverb_interf_performance_8d8a980436/']
+dir = ['data/20221208-164354_reverb_interf_performance_d6e9a4e599/']
 
 
 import json
