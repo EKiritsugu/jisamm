c3ddee19e4172cb86633e726c443fe347eef2316
AALab_linziyao <46914247+EKiritsugu@users.noreply.github.com>
2022-12-08 22:30:52 +0800

    .

--- plot_test.py
+++ plot_test.py
@@ -1,5 +1,5 @@
 # "C:/Users/aalab_linziyao/Documents/GitHub/jisamm/data/20221207-180003_reverb_interf_performance_2fd4afb7fe/data.json"
-dir = ['data/20221208-164354_reverb_interf_performance_d6e9a4e599/']
+dir = ['data/20221208-221054_reverb_interf_performance_179d328fac/']
 
 
 import json
@@ -7,7 +7,7 @@ from data_loader import load_data
 
 df, rt60, parameters = load_data(dir)
 
-print(df)
+# print(df)
 
 df.to_csv(dir[0] + 'out.csv')
 # with open(dir, 'r') as file:
