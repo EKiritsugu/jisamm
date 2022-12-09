7258618a80623aa44f89cb45dee416502c5fedb0
AR <46914247+EKiritsugu@users.noreply.github.com>
2022-12-09 14:28:11 +0800

    .

--- bss/ilrma_t_function.py
+++ bss/ilrma_t_function.py
@@ -7,6 +7,7 @@ import soundfile as sf
 from nara_wpe.utils import stft, istft
 # from .projection_back import projection_back
 import numpy as np
+# from projection_back import project_back
 ######################################################################################################################
 
 def hermite(x):
@@ -215,7 +216,7 @@ def ilrma_t_ip(
     delay = 2,
     callback=None,
     callback_checkpoints=[],
-        return_filters=False,
+    return_filters=False,
     ):
     """
      X: ndarray (nframes, nfrequencies, nchannels)
@@ -328,8 +329,6 @@ def ilrma_t_ip(
             callback(P[:,:n_src,:n_src], Y_tmp, "laplace")
 
     Y = Y.transpose([2, 0, 1]).copy()
-    z = projection_back(Y, X_original[:, :, 0])
-    Y *= np.conj(z[None, :, :])
 
     if return_filters:
         return Y, W
@@ -477,8 +476,6 @@ def ilrma_t_iss_seq(
 
 
     Y = Y.transpose([2, 0, 1]).copy()
-    z = projection_back(Y, X_original[:, :, 0])
-    Y *= np.conj(z[None, :, :])
 
     if return_filters:
         return Y, W
@@ -644,8 +641,6 @@ def ilrma_t_iss_joint(
 
     # print('bp')
     Y = Y.transpose([2, 0, 1]).copy()
-    z = projection_back(Y, X_original[:, :, 0])
-    Y *= np.conj(z[None, :, :])
 
 
     if return_filters:
@@ -656,126 +651,6 @@ def ilrma_t_iss_joint(
 
 
 # 怎么看都是，ilrma算法用了不如不用，直接拿iva似乎更好
-
-def my_ilrma_t(X, n_components = 2, n_iter = 20, taps = 5, delay = 2):
-    """
-     X: ndarray (nframes, nfrequencies, nchannels)
-     Returns an (nframes, nfrequencies, nsources) array.
-     """
-    n_frames, n_freq, n_chan = X.shape
-
-    # default to determined case
-
-    n_src = X.shape[2]
-
-    # Only supports determined case
-    assert n_chan == n_src, "There should be as many microphones as sources"
-
-    W = np.array([np.eye(n_chan, n_src) for f in range(n_freq)], dtype=X.dtype)
-
-    # initialize the nonnegative matrixes with random values
-    T = np.array(0.1 + 0.9 * np.random.rand(n_src, n_freq, n_components))
-    V = np.array(0.1 + 0.9 * np.random.rand(n_src, n_frames, n_components))
-    R = np.zeros((n_src, n_freq, n_frames))
-
-    lambda_aux = np.zeros(n_src)
-    eps = 1e-15
-    eyes = np.tile(np.eye(n_chan, n_chan), (n_freq, 1, 1))
-
-    # Things are more efficient when the frequencies are over the first axis
-    Y = np.zeros((n_freq, n_src, n_frames), dtype=X.dtype)
-    Y_wpe = np.zeros((n_freq, n_src, n_frames), dtype=X.dtype)
-    X_original = X
-
-    X = X.transpose([1, 2, 0]).copy()
-    # print(np.shape(X))  # n_freq, n_ch, n_frames
-
-    np.matmul(T, V.swapaxes(1, 2), out=R)
-    # Compute the demixed output
-
-    # Y2.shape == R.shape == (n_src, n_freq, n_frames)
-    Y2 = np.power(abs(Y.transpose([1, 0, 2])), 2.0)
-    iR = 1 / R
-
-    ##WPE
-    X_line = build_y_tilde(Y, taps, delay)
-    X_tilde = np.array([np.vstack((X[i], X_line[i]))
-                        for i in range(n_freq)])
-    P = np.zeros((n_freq, n_src, X_tilde.shape[1]), dtype=X.dtype)
-    W_p = P[:, :, :n_src]
-    W_p[:, :, :] = np.array([np.eye(n_chan, n_src) for f in range(n_freq)], dtype=X.dtype)
-    A_p = np.zeros((n_freq, X_tilde.shape[1]), dtype=X.dtype)
-    eyes_t = np.tile(np.eye(X_tilde.shape[1], X_tilde.shape[1], dtype=X.dtype), (n_freq, 1, 1))
-
-    # print(P[0,:,:])
-    # print(np.shape(X_tilde))
-
-    def demix(Y, X, W):
-        Y[:, :, :] = np.matmul(W, X)
-
-    demix(Y, X, W)
-
-    def demix_t(Y_wpe, X_tilde, P):
-        Y_wpe[:, :, :] = np.matmul(P, X_tilde)
-
-    demix_t(Y_wpe, X_tilde, P)
-    ###########
-
-    for epoch in range(n_iter):
-        # if epoch%10==0:
-        #     print(epoch)
-        # simple loop as a start
-        for s in range(n_src):
-            ## NMF
-            ######
-            T[s, :, :] *= np.sqrt(np.dot(Y2[s, :, :] * iR[s, :, :] ** 2, V[s, :, :]) / np.dot(iR[s, :, :], V[s, :, :]))
-            T[T < eps] = eps
-            R[s, :, :] = np.dot(T[s, :, :], V[s, :, :].T)
-            R[R < eps] = eps
-            iR[s, :, :] = 1 / R[s, :, :]
-            V[s, :, :] *= np.sqrt(
-                np.dot(Y2[s, :, :].T * iR[s, :, :].T ** 2, T[s, :, :]) / np.dot(iR[s, :, :].T, T[s, :, :]))
-            V[V < eps] = eps
-            R[s, :, :] = np.dot(T[s, :, :], V[s, :, :].T)
-            R[R < eps] = eps
-            iR[s, :, :] = 1 / R[s, :, :]
-            ##########
-
-            ## WPE
-            V_wpe = np.matmul((X_tilde * iR[s, :, None, :]), np.conj(X_tilde.swapaxes(1, 2))) / n_frames
-
-            A_p[:, :n_src] = np.linalg.solve(W_p, eyes[:, :, s])
-
-            V_inv = _stable_solve(V_wpe, eyes_t)
-            P[:, s, :] = np.conj(_stable_solve(V_wpe, A_p[:, :, None])).squeeze()
-            denom_wpe = np.conj(A_p[:, None, :]) @ V_inv @ A_p[:, :, None]
-            P[:, s, :] /= np.sqrt(denom_wpe[:, :, 0])
-
-
-        demix_t(Y, X_tilde, P)
-        np.power(abs(Y.transpose([1, 0, 2])), 2.0, out=Y2)
-
-        for s in range(n_src):
-            lambda_aux[s] = 1 / np.sqrt(np.mean(Y2[s, :, :]))
-            W[:, :, s] *= lambda_aux[s]
-            Y2[s, :, :] *= lambda_aux[s] ** 2
-            R[s, :, :] *= lambda_aux[s] ** 2
-            T[s, :, :] *= lambda_aux[s] ** 2
-
-        from nara_wpe.wpe import wpe
-
-        Y = wpe(Y,taps=taps,
-                    delay=delay,
-                    iterations=1,
-                    statistics_mode='full'
-                )
-
-    Y = Y.transpose([2, 0, 1]).copy()
-    z = projection_back(Y, X_original[:, :, 0])
-    Y *= np.conj(z[None, :, :])
-
-    return Y
-
 
 
 
