e7461db3a70cb3f069e6f63712b0e649d21e875c
AR <46914247+EKiritsugu@users.noreply.github.com>
2022-12-10 10:49:02 +0800

    测试了双声源

--- plot_test.py
+++ plot_test.py
@@ -1,5 +1,5 @@
 # "C:/Users/aalab_linziyao/Documents/GitHub/jisamm/data/20221207-180003_reverb_interf_performance_2fd4afb7fe/data.json"
-dir = ['data/20221208-221054_reverb_interf_performance_179d328fac/']
+dir = ['data/20221209-205516_reverb_interf_performance_dd0373ae97/']
 
 
 import json
